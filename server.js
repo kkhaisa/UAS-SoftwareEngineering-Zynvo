da81595b4b708bce7a5ace13903eae62856cfb3d
naylaputricahyaramadani-sudo <naylaputricahyaramadani@gmail.com>
2026-06-23 22:56:48 +0700

    feat: upload ulang server.js dan index.html versi paling baru

--- server.js
+++ server.js
@@ -4,9 +4,38 @@ const bcrypt = require("bcryptjs");
 const app = express();
 
 app.use(express.json());
+// BARU: Baris sakti agar folder 'public' (login.html dan index.html) bisa dibuka di browser
+app.use(express.static("public"));
 
 const JWT_SECRET = "ZYNVO_SUPER_SECRET_KEY_2026";
 const usersDatabase = [];
+
+const eventsDatabase = [
+  {
+    id: 1,
+    nama_event: "Seminar AI & Machine Learning",
+    tanggal: "2026-07-15",
+    lokasi: "Auditorium UPJ",
+    kategori: "Seminar",
+    kuota: 200,
+  },
+  {
+    id: 2,
+    nama_event: "Workshop UI/UX",
+    tanggal: "2026-07-20",
+    lokasi: "Lab Komputer",
+    kategori: "Workshop",
+    kuota: 100,
+  },
+  {
+    id: 3,
+    nama_event: "Build Club Web Development",
+    tanggal: "2026-07-25",
+    lokasi: "Ruang A301",
+    kategori: "Workshop",
+    kuota: 80,
+  },
+];
 
 app.post("/api/register", async (req, res) => {
   try {
@@ -16,11 +45,9 @@ app.post("/api/register", async (req, res) => {
       !email_kampus.endsWith("@upj.ac.id") &&
       !email_kampus.endsWith("@student.upj.ac.id")
     ) {
-      return res
-        .status(400)
-        .json({
-          message: "Registrasi gagal. Harus menggunakan email resmi kampus!",
-        });
+      return res.status(400).json({
+        message: "Registrasi gagal. Harus menggunakan email resmi kampus!",
+      });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -65,6 +92,15 @@ app.post("/api/login", async (req, res) => {
     message: "Login berhasil!",
     token: token,
     role: user.role,
+  });
+});
+
+
+app.get("/api/events", (req, res) => {
+  res.status(200).json({
+    message: "Daftar event berhasil diambil",
+    total_event: eventsDatabase.length,
+    data: eventsDatabase,
   });
 });
 
